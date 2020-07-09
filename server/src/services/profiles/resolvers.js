11d8c12e460dc04a44f184982b77918bd2f914b0
Kah Wai LIM <kahwai222@gmail.com>
2020-07-10 01:54:23 +0800

    Chapter 6

--- server/src/services/profiles/resolvers.js
+++ server/src/services/profiles/resolvers.js
@@ -17,7 +17,10 @@ const resolvers = {
       return { __typename: 'Account', id: profile.accountId };
     },
     following(profile, args, { dataSources }, info) {
-      return dataSources.profilesAPI.getFollowedProfiles(profile.following);
+      return dataSources.profilesAPI.getFollowedProfiles({
+        ...args,
+        following: profile.following,
+      });
     },
     id(profile, args, context, info) {
       return profile._id;
@@ -37,10 +40,19 @@ const resolvers = {
       return profile;
     },
     profiles(parent, args, { dataSources }, info) {
-      return dataSources.profilesAPI.getProfiles();
+      return dataSources.profilesAPI.getProfiles(args);
     },
-    searchProfiles(parent, { query: { text } }, { dataSources }, info) {
-      return dataSources.profilesAPI.searchProfiles(text);
+    searchProfiles(
+      parent,
+      { after, first, query: { text } },
+      { dataSources },
+      info,
+    ) {
+      return dataSources.profilesAPI.searchProfiles({
+        after,
+        first,
+        searchString: text,
+      });
     },
   },
 
